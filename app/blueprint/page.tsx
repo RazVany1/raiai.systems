f961a2bc48d121b5152f4cd46ee56dfac2ddeb5c
RazVany1 <allyoucandodaily@gmail.com>
2025-10-26 20:55:53 +0200

    Update page.tsx

--- app/blueprint/page.tsx
+++ app/blueprint/page.tsx
@@ -2,17 +2,36 @@
 import { useState } from "react";
 import IntelligenceCore from "../../components/IntelligenceCore";
 import ControlDock from "../../components/ControlDock";
+import DiagnosticsSidebar from "../../components/DiagnosticsSidebar";
+import BlueprintScene from "../../components/BlueprintScene";
+import PulseMap from "../../components/PulseMap";
+import ModuleGrid from "../../components/ModuleGrid";
 
 export default function BlueprintPage() {
   const [coreSoundEnabled, setCoreSoundEnabled] = useState(false);
 
   return (
     <main className="relative min-h-screen bg-black overflow-hidden text-cyan-400 font-mono">
-      <IntelligenceCore coreSoundEnabled={coreSoundEnabled} />
+      {/* STRATURILE VIZUALE */}
+      <div className="absolute inset-0 z-0">
+        <BlueprintScene />
+        <PulseMap />
+        <ModuleGrid />
+      </div>
 
-      {/* DOCK fixat în față */}
-      <div className="absolute bottom-6 left-6 z-[99999]">
+      {/* CONȚINUT PRINCIPAL */}
+      <div className="relative z-10 flex items-center justify-center h-full">
+        <IntelligenceCore coreSoundEnabled={coreSoundEnabled} />
+      </div>
+
+      {/* CONTROL DOCK (STÂNGA JOS) */}
+      <div className="absolute bottom-6 left-6 z-50">
         <ControlDock onToggleCoreSound={setCoreSoundEnabled} />
+      </div>
+
+      {/* DIAGNOSTICS (DREAPTA SUS) */}
+      <div className="absolute top-6 right-6 z-40">
+        <DiagnosticsSidebar />
       </div>
     </main>
   );
